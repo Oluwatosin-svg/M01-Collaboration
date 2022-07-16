9ae8d62985c248f9825d053d2941d2a59367eb74
Oluwatosin-svg <75536799+Oluwatosin-svg@users.noreply.github.com>
2022-07-16 11:02:16 -0400

    Add files via upload

---
+++ mp.py
@@ -0,0 +1,31 @@
+import multiprocessing
+import time
+import datetime
+import os
+
+start = time.time()
+Firstprocess = ()
+Secongprocess = ()
+Thirdprocess = ()
+
+print(datetime.datetime.now())
+if __name__=="__main__":
+    
+    
+    
+    p1 = multiprocessing.Process(target=Firstprocess)
+    p2 = multiprocessing.Process(target=Secongprocess)
+    p3 = multiprocessing.Process(target=Thirdprocess)
+
+    p1.start()
+    p2.start()
+    p3.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+
+end = time.time()
+    
+print("Waited for " +str(end-start)+" seconds")
+
